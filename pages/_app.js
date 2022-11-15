da2d5383bee300a443ec1a2323da61ecc5d1aa0c
Tran Minh Nhut <nhuttm@dgroup.co>
2022-11-15 16:14:21 +0700

    feat: create example integrate with nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
-import '../styles/globals.css'
+import React, { useState, useEffect } from 'react'
+import { useRouter } from "next/router";
+
+import auth from '../auth';
+import '../styles/App.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [data, setData] = useState({});
+  const router = useRouter();
+  useEffect(() => {
+    setData(JSON.parse(window.localStorage.getItem('data')) || {});
+  }, []);
+
+  const logout = async () => {
+    const authIns = await auth();
+    const logoutHandler = (err) => {
+      // return error when logout, receive error which mean logout lumin service success
+      if (err) {
+        setData({});
+        localStorage.removeItem('data');
+        router.push('/login');
+      }
+      authIns.off('logout', logoutHandler)
+    }
+    authIns.on('logout', logoutHandler)
+    authIns.logout('lumin');
+  }
+
+  return (
+    <div className="App">
+      <h1>Lumin 3rd Party Integrate</h1>
+      <h2>Third party integrate demo app</h2>
+      <Component data={data} setData={setData} {...pageProps} />
+      { data.accessToken && <button className="AppButton" style={{ marginTop: 10 }} onClick={logout}>Logout</button> }
+    </div>
+  )
 }
 
 export default MyApp
