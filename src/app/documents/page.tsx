ecac469f87cb6a53313cacb260df748b0f77fd7e
Tran Minh Nhut <nhuttm@dgroup.co>
2023-02-09 16:30:16 +0700

    feat: integrate with send and sign button

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -1,6 +1,17 @@
 'use client'
-import React, { useEffect } from 'react';
-import { Avatar, Button, HStack, Link, Text, VStack, Box, Image, Divider } from '@chakra-ui/react'
+import React, { useState, useEffect, useRef } from 'react';
+import { useRouter } from 'next/navigation'
+import { get, set, del } from 'idb-keyval';
+import {
+  Avatar,
+  Button,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverBody,
+  Portal,
+  Spinner,
+  HStack, Link, Text, VStack, Box, Image, Divider } from '@chakra-ui/react'
 import Logo from "@/components/Logo";
 import "./documents.scss";
 
@@ -19,23 +30,73 @@ const Filters = [
 ]
 
 export default function Document() {
+  const [pageLoaded, setPageLoaded] = useState(false);
+  const inputRef = useRef(null);
+  const [fileList, setFileList] = useState<{
+    fileName: string, file: File, stat: number, day: number, status: number, price: number
+  }[]>([]); 
+  const [sendAndSignLoading, setSendAndSignLoading] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
-//     Array(10).fill(0).map((_, i) => {
-//       console.log("🚀 ~ file: page.tsx ~ line 31 ~ Array ~ window.lumin", window.lumin.auth)
-//       window.lumin.sign
-// .renderButton({
-//         id: i.toString(),
-//         size: 'small'
-//       })
-//     });
-// window.lumin.sign
-// .renderButton({
-//         id: 0,
-//         size: 'small'
-//       })
-  }, []);
-  return (
+    const loadData = async () => {
+      const fileList = await get('file-list') || [];
+      setFileList(fileList);
+      setPageLoaded(true);
+    }
+    if (!localStorage.getItem('id_token')) {
+      router.push('/auth/signin');
+    } else {
+      loadData();
+    }
+  }, [])
+
+  const convertFileToBlob = (file: File) => {
+    return new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        const blob = new Blob([new Uint8Array(e.target.result)], {type: file.type });
+        resolve(blob)
+      };
+      reader.onerror = function() {
+        resolve(file);
+      }
+      reader.readAsArrayBuffer(file);
+    })
+  };
+
+  const addFiles = async (files) => {
+    const newFiles = [];
+    for (const file of files) {
+      const blobFile = await convertFileToBlob(file);
+      const fileData = {
+        file: blobFile,
+        fileName: file.name,
+        stat: Math.floor(Math.random() * (999 - 111) + 111),
+        day: Math.floor(Math.random() * (6 - 1) + 1),
+        status: Math.floor(Math.random() * (31 - 1) + 1),
+        price: Math.floor(Math.random() * (9999 - 2000) + 2000)
+      };
+      newFiles.push(fileData);
+    }
+    const newFileList = [...fileList, ...newFiles];
+    set('file-list', newFileList)
+      .finally(() => {
+        setFileList(newFileList);
+      })
+  }
+
+  const sendAndSign = (file, fileName) => {
+    window.lumin.sign.sendAndSign({
+      accessToken: localStorage.getItem('access_token'),
+      fileData: file,
+      fileName,
+      onError: (e) => console.log(e),
+      onLoading: setSendAndSignLoading,
+    }) 
+  }
+
+  return !pageLoaded ? <div></div> : (
     <div className="Document">
       <div className="Document__Header">
         <div className="Document__Logo">
@@ -64,7 +125,22 @@ export default function Document() {
             ))
           }
         </HStack>
-        <Avatar width={50} height={50} src="https://picsum.photos/200/300"/>
+        <Popover>
+          <PopoverTrigger>
+            <Avatar width={50} height={50} src="https://picsum.photos/200/300"/>
+          </PopoverTrigger>
+          <Portal>
+            <PopoverContent>
+              <PopoverBody>
+                <Button colorScheme='blue' onClick={() => {
+                  del('file-list');
+                  localStorage.clear();
+                  router.push('/auth/signin');
+                }}>Logout</Button>
+              </PopoverBody>
+            </PopoverContent>
+          </Portal>
+        </Popover>
       </div>
       <div className="Document__Body">
         <div className="Document__Body--header">
@@ -98,6 +174,23 @@ export default function Document() {
               }
             </HStack>
           </HStack>
+          {/* <Input 
+            type="file"
+            onChange={(e) => {
+              window.lumin.sign.sendAndSign({
+                accessToken: localStorage.getItem('access_token'),
+                fileData: e.target.files[0],
+                fileName: 'test'
+              })
+            }}
+          /> */}
+          <input
+            onChange={(e) => addFiles(e.target.files)}
+            type='file'
+            accept="application/pdf"
+            multiple
+            ref={inputRef}
+            hidden />
           <Button
             border="1px solid"
             fontWeight={500}
@@ -105,6 +198,7 @@ export default function Document() {
             borderRadius={12}
             colorScheme='messenger'
             minWidth={190}
+            onClick={() => inputRef.current?.click()}
             leftIcon={<span style={{ fontSize: 20 }}>+</span>}
             borderColor="primary.100">
             List a Property
@@ -127,16 +221,16 @@ export default function Document() {
           </HStack>
           <VStack bg="#ffffff" height="100%" padding="0 40px" overflowY="scroll">
              {
-              Array(10).fill(0).map((_, i) => (
+             fileList.map((file, i) => (
                 <React.Fragment key={i}>
                 <HStack width="100%" paddingTop={5} paddingBottom={i === 9 ? 5 : 0}>
                   <Box width="30%">
                     <HStack>
                       <Image alt="prod" src="https://picsum.photos/200/300" width={20} height={20} borderRadius={8}/>
                       <VStack alignItems="flex-start">
-                        <Text fontWeight={500} fontSize={16} color="primary.100">Californian Living</Text>
+                        <Text fontWeight={500} fontSize={16} color="primary.100">{file.fileName}</Text>
                         <Text fontWeight={500} fontSize={10} color="primary.80">41 Esplanade</Text>
-                        <Text fontWeight={500} fontSize={10} color="primary.80">$5500 p/m</Text>
+                        <Text fontWeight={500} fontSize={10} color="primary.80">{file.price} p/m</Text>
                       </VStack>
                     </HStack>
                   </Box>
@@ -154,7 +248,7 @@ export default function Document() {
                     <VStack alignItems="flex-start">
                       <HStack>
                         <Image alt="arrow" src="/assets/arrow.svg" width="14px" height="14px" borderRadius={8}/>
-                        <Text fontWeight={500} fontSize={14} color="primary.100">245</Text>
+                        <Text fontWeight={500} fontSize={14} color="primary.100">{file.stat}</Text>
                       </HStack>
                       <Text fontWeight={500} fontSize={10} color="primary.100">Total view</Text>
                     </VStack>
@@ -162,16 +256,20 @@ export default function Document() {
                   <Box width="20%">
                     <VStack alignItems="flex-start">
                       <Text fontWeight={500} fontSize={14} color="primary.100">6 Aug | 12:20pm</Text>
-                      <Text fontWeight={500} fontSize={10} color="primary.100">6 days ago</Text>
+                      <Text fontWeight={500} fontSize={10} color="primary.100">{file.day} days ago</Text>
                     </VStack>
                   </Box>
                   <Box width="10%">
-                    <Text fontWeight={500} fontSize={10} color="primary.100">Till 6 Dec</Text>
+                    <Text fontWeight={500} fontSize={10} color="pr  imary.100">Till {file.status} Dec</Text>
                   </Box>
                   <Box width="20%">
                     <HStack alignItems="flex-start">
                       <Image alt="edit" src="/assets/edit.svg" width="54px" height="32px" borderRadius={8}/>
-                      <lumin-sign size="small"/>
+                      {
+                        sendAndSignLoading ? <Spinner width={8} height={8}/> : (
+                          <lumin-sign size="small" onClick={() => sendAndSign(file.file, file.fileName)}/>
+                        )
+                      }
                     </HStack>
                   </Box>
                 </HStack>
